795817ff70cf8d314b815135ada0625935e1d70e
Giorgos Papasteriadis <gpapasteriadis@terracom.gr>
2021-06-10 15:06:03 +0300

    Implemented rabbitmq communication between microservices

---
+++ src/Services/Ordering/Ordering.API/Mappings/OrderingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using EventBus.Messages.Events;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+using Ordering.Domain.Entities;
+
+
+namespace Ordering.API.Mappings
+{
+    public class OrderingProfile : Profile
+    {
+        public OrderingProfile()
+        {
+            CreateMap<CheckoutOrderCommand, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
